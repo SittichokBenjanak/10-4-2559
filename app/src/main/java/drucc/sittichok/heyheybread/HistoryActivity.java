d3b6b6c411abbb06186c188b41fd126afb602761
sittichok <moszajang@outlook.com>
2016-04-26 22:31:40 +0700

    เริ่ม สถานะที่ดีที่สุด 26/4/59 22:31น.

--- app/src/main/java/drucc/sittichok/heyheybread/HistoryActivity.java
+++ app/src/main/java/drucc/sittichok/heyheybread/HistoryActivity.java
@@ -11,8 +11,8 @@ public class HistoryActivity extends AppCompatActivity {
 
     // Explicit
     private String strID;
-    private ListView orderUser;
-    private String OrderNumber, DateOrder, SumPrice, Status;
+    private ListView orderUserListView;
+    private String OrderNumberString, DateOrderString, SumPriceString, StatusString;
 
 
 
@@ -22,62 +22,20 @@ public class HistoryActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_history);
 
+        strID = getIntent().getStringExtra("ID");
         bindWidget();
-
-        readOrder();
-
-        showView();
 
 
     }   // onCreate
 
-    private void readOrder() {
 
-        SQLiteDatabase objSqLiteDatabase = openOrCreateDatabase(MyOpenHelper.DATABASE_NAME,
-                MODE_PRIVATE, null);
-        Cursor objCursor = objSqLiteDatabase.rawQuery("SELECT * FROM tborder WHERE _id = " + "'" + strID + "'", null);
-        objCursor.moveToFirst();
-        OrderNumber = objCursor.getString(objCursor.getColumnIndex(ManageTABLE.COLUMN_id));
-        DateOrder = objCursor.getString(objCursor.getColumnIndex(ManageTABLE.COLUMN_OrderDate));
-        SumPrice = objCursor.getString(objCursor.getColumnIndex(ManageTABLE.COLUMN_GrandTotal));
-        Status = objCursor.getString(objCursor.getColumnIndex(ManageTABLE.COLUMN_Status));
-
-        String[] OrderNumber = new String[objCursor.getCount()];
-        String[] DateOrder = new String[objCursor.getCount()];
-        String[] SumPrice = new String[objCursor.getCount()];
-        String[] status = new String[objCursor.getCount()];
-
-        for (int i=0; i<objCursor.getCount(); i++) {
-
-            OrderNumber[i] = objCursor.getString(objCursor.getColumnIndex(ManageTABLE.COLUMN_id));
-            DateOrder[i] = objCursor.getString(objCursor.getColumnIndex(ManageTABLE.COLUMN_Date));
-            SumPrice[i] = objCursor.getString(objCursor.getColumnIndex(ManageTABLE.COLUMN_GrandTotal));
-            status[i] = objCursor.getString(objCursor.getColumnIndex(ManageTABLE.COLUMN_Status));
-
-            objCursor.moveToNext();
-
-        }   // for
-        objCursor.close();
-
-    }   //  readOrder
 
     private void bindWidget() {
 
-        orderUser = (ListView) findViewById(R.id.listView3);
+        orderUserListView = (ListView) findViewById(R.id.listView3);
 
     }   // bindWidget
 
 
-    private void showView() {
 
-        strID = getIntent().getStringExtra("ID");
-        SQLiteDatabase sqLiteDatabase = openOrCreateDatabase(MyOpenHelper.DATABASE_NAME,
-                MODE_PRIVATE, null);
-        Cursor cursor = sqLiteDatabase
-                .rawQuery("SELECT * FROM tborder WHERE _id = " + "'" + strID + "'", null);
-
-
-        cursor.close();
-
-    }   // showView
 }   // Main Class
