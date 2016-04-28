d15caebff531f0147ac54ca9f2e2cc9cf3fc52d7
sittichok <moszajang@outlook.com>
2016-04-28 14:18:56 +0700

    รีเฟส Hub ได้แล้ว

--- app/src/main/java/drucc/sittichok/heyheybread/HubActivity.java
+++ app/src/main/java/drucc/sittichok/heyheybread/HubActivity.java
@@ -3,10 +3,24 @@ package drucc.sittichok.heyheybread;
 import android.content.Intent;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
+import android.os.StrictMode;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.ImageView;
+
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 
 public class HubActivity extends AppCompatActivity implements View.OnClickListener {
 
@@ -20,13 +34,89 @@ public class HubActivity extends AppCompatActivity implements View.OnClickListen
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_hub);
+        deleteOrder();
+        syntborder();
+
         //Bind Widget
         bindWidget();
 
         //Image Controller
         imageController();
     }   // onCreate
+    private void syntborder() {
+        StrictMode.ThreadPolicy threadPolicy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
+        StrictMode.setThreadPolicy(threadPolicy);
+        int intTimes = 1;
+        while (intTimes <= 1) {
+            InputStream objInputStream = null;
+            String strJSON = null;
+            String strURLtborder = "http://swiftcodingthai.com/mos/php_get_tborder_mos.php";
+            HttpPost objHttpPost = null;
+            // 1 Create InputStream
+            try {
+                HttpClient objHttpClient = new DefaultHttpClient();
+                switch (intTimes) {
+                    case 1:
+                        objHttpPost = new HttpPost(strURLtborder);
+                        break;
+                }   // switch
 
+                HttpResponse objHttpResponse = objHttpClient.execute(objHttpPost);
+                HttpEntity objHttpEntity = objHttpResponse.getEntity();
+                objInputStream = objHttpEntity.getContent();
+
+            } catch (Exception e) {
+                Log.d("sss", "InputStream ==> " + e.toString());
+            }
+            // 2 Create JSON String
+            try {
+                BufferedReader objBufferedReader = new BufferedReader(new InputStreamReader(objInputStream, "UTF-8"));
+                StringBuilder objStringBuilder = new StringBuilder();
+                String strLine = null;
+                while ((strLine = objBufferedReader.readLine()) != null) {
+                    objStringBuilder.append(strLine);
+                }
+                objInputStream.close();
+                strJSON = objStringBuilder.toString();
+
+            } catch (Exception e) {
+                Log.d("sss", "InputStream ==> " + e.toString());
+            }
+
+            // 3 Update JSON String to SQLite
+            try {
+                JSONArray objJsonArray = new JSONArray(strJSON);
+                for (int i=0; i<objJsonArray.length(); i++) {
+                    JSONObject object = objJsonArray.getJSONObject(i);
+                    switch (intTimes) {
+                        case 1:
+                            ManageTABLE objManageTABLE = new ManageTABLE(this);
+                            String strOrderDate = object.getString(ManageTABLE.COLUMN_OrderDate);
+                            String strCustomerID = object.getString(ManageTABLE.COLUMN_CustomerID);
+                            String strGrandTotal = object.getString(ManageTABLE.COLUMN_GrandTotal);
+                            String strStatus1 = object.getString(ManageTABLE.COLUMN_Status);
+                            objManageTABLE.addtbOrder(strOrderDate, strCustomerID, strGrandTotal, strStatus1);
+                            break;
+                    }
+
+                }   // for
+
+            } catch (Exception e) {
+                Log.d("sss", "InputStream ==> " + e.toString());
+            }
+            intTimes += 1;
+
+        }   // while
+
+
+    }   // syntborder
+
+    private void deleteOrder() {
+        SQLiteDatabase objSqLiteDatabase = openOrCreateDatabase(MyOpenHelper.DATABASE_NAME,
+                MODE_PRIVATE, null);
+        objSqLiteDatabase.delete(ManageTABLE.TABLE_TBORDER,null,null);
+
+    }   // deleteOrder
 
 
     private void imageController() {
@@ -75,13 +165,28 @@ public class HubActivity extends AppCompatActivity implements View.OnClickListen
 
             case R.id.imageView6:
                 // OrderHistory
-                Intent intent2 = new Intent(HubActivity.this, HistoryActivity.class);
-                intent2.putExtra("ID", idString);
-                startActivity(intent2);
+                checkHistory();
+
 
                 break;
         }   //switch
     }   // onClick
+
+    private void checkHistory() {
+        SQLiteDatabase objSqLiteDatabase = openOrCreateDatabase(MyOpenHelper.DATABASE_NAME,
+                MODE_PRIVATE, null);
+        Cursor objCursor = objSqLiteDatabase.rawQuery("SELECT * FROM tborder WHERE CustomerID = " + idString, null);
+        if (objCursor.getCount() > 0) {
+            Intent intent2 = new Intent(HubActivity.this, HistoryActivity.class);
+            intent2.putExtra("ID", idString);
+            startActivity(intent2);
+        } else {
+            MyAlertDialog objMyAlertDialog = new MyAlertDialog();
+            objMyAlertDialog.errorDialog(HubActivity.this,"ยังไม่มีประวัติการสั่งซื้อ","กรุณาสั่งสินค้าก่อนครับ");
+        }
+
+
+    }   // checkHistory
 
     private void clickReadOrder() {
         SQLiteDatabase objSqLiteDatabase = openOrCreateDatabase(MyOpenHelper.DATABASE_NAME,
